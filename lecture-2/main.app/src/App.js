6cf80b33ce77e6233b55726e02a4728cb84ce8d6
Bhautik Dudhat <bdudhat293@gmail.com>
2024-07-01 12:08:25 +0530

    componet

---
+++ lecture-2/main.app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// import Header (2 types- line 5,6)  
+
+// import Header from './componate/Header';
+import{Header} from './componate/Header'
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ lecture-2/main.app/src/componate/Header.js
@@ -0,0 +1,12 @@
+
+// Create Header ecomponet
+
+function Header() {
+    return (
+        <h1>Welcome to web browser</h1>
+    )
+}
+
+// Export
+// export default Header;
+export {Header};
