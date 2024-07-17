00145bf324fd4cd8317f4c524c8fe33e46aaa130
Bhautik dudhat <bdudhat293@gmail.com>
2024-07-17 10:33:59 +0530

    react

---
+++ React_js/Yarn_installation/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Profile from './Component/Export_the_component'
+
+function App() {
+  return (
+    <div className="App">
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React_js/Yarn_installation/src/Component/Export_the_component.js
@@ -0,0 +1,9 @@
+function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/lICfvbD.jpg"
+        alt="Aklilu Lemma"
+      />
+    );
+  }
+  export default Profile;
